260ecf4eb5cd45d28ca76a0761515470b11b80f7
joceamayar <jocelin@tavlee.com>
2023-10-09 23:46:42 -0400

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,4 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
+
+// mongodb+srv://booksearch:booksearch@booksearchcluster.0qrgozy.mongodb.net/?retryWrites=true&w=majority
+
 module.exports = mongoose.connection;
